756ada6cd90fcb1a5d292263051b34767ea9473d
kram3r30 <alister.mark3r@gmail.com>
2023-02-23 18:39:12 -0500

    modals-trial

--- app/js/index.js
+++ app/js/index.js
@@ -27,11 +27,50 @@ el.innerHTML = recipeName;
 
 // ==
 
-// var modal = document.querySelector('.modal')
+// document.addEventListener("click", showPopover);
+
+// // I tried the below first
+// var modalOuter = document.querySelector(".modal-outer");
+// var modalOuterPickit = document.querySelector(".modal-outerpickit");
+
+// function showPopover(event) {
+//   if (event.target.matches(".beta")) {
+//     modalOuter.classList.add("open");
+//   } else if (event.target.matches(".nav-pickit")) {
+//     modalOuterPickit.classList.add("open");
+//   } else if (event.target.matches(".closer, .modal-outerpickit")) {
+//     modalOuterPickit.classList.remove("open");
+//   } else if (event.target.matches(".closer, .modal-outer")) {
+//     modalOuterPickit.classList.remove("open");
+//   } else {
+//     return;
+//   }
+//   event.preventDefault();
+// }
+
+// document.addEventListener("click", showPopover);
+
+//==
+
 var modalOuter = document.querySelector(".modal-outer");
+var modalInner = document.querySelector(".modal");
+
+var betaContent = `
+<h3>Oooops!</h3>
+<p>Wow! Nothing works!<p>
+`;
+var buttonContent = `
+<h2>Coming Soon</h2>
+<p>This feature coming soon.<p>
+<a class="closer" href="#0">✖︎</a>
+`;
 
 function showPopover(event) {
   if (event.target.matches(".beta")) {
+    modalInner.innerHTML = betaContent;
+    modalOuter.classList.add("open");
+  } else if (event.target.closest("nav ul")) {
+    modalInner.innerHTML = buttonContent;
     modalOuter.classList.add("open");
   } else if (event.target.matches(".closer, .modal-outer")) {
     modalOuter.classList.remove("open");
